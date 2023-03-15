9b8fbe13c2a2e7c138f4ea70b6e028163d058141
Eugene Ilin <eugenepianist@gmail.com>
2023-03-14 20:58:34 -0400

    basic structure started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
-      <h1>Captured Memories by Liz</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSlider from '../components/Home/HeroSlider';
+
+const Home = () => {
+  return (
+    <section id='home' className='container'>
+      <HeroSlider />
+    </section>
+  );
+};
+
+export default Home;

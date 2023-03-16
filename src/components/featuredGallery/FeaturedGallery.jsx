ccb641ec8db924a8c9986e0c89d929a767bb1fcd
Eugene Ilin <eugenepianist@gmail.com>
2023-03-15 20:57:28 -0400

    good press praise the Lord

---
+++ src/components/featuredGallery/FeaturedGallery.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './featuredGallery.css';
+
+const FeaturedGallery = () => {
+  return <div id='featured-gallery'></div>;
+};
+
+export default FeaturedGallery;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='container'>
+        <h4>This is a Footer</h4>
+        <h5>Add content above me and I'll stay out of your way.</h5>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
